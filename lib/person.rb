0f4e66f741ff2fa1a9387596839ac949763a31ad
I-Am-Lain <vetter.william@gmail.com>
2020-09-16 06:22:20 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+    def name=(that_bois_name)
+        @name = that_bois_name
+    end
+
+    def name
+        @name
+    end
+
+    def job=(that_bois_job)
+        @job = that_bois_job
+    end
+
+    def job
+        @job
+    end
+end
